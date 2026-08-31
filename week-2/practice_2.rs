4ef79e097cbab68373f568782732fee807c9bf1b
Miracle-Aduma <miracle.aduma@pau.edu.ng>
2026-08-31 11:41:50 +0100

    practice_2

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+	println!(); 
+	println!("hello"); //prints hello
+	println!("format {} arguments", "some"); // prints format some arguments
+	println!("My name is {}. I am a member of Computer Science", "Aduma Miracle Ugwo");
+}
