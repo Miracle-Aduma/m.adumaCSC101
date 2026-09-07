dd4d0ade409881e753ff6fb4c6b1c3a786fe0adb
Miracle-Aduma <miracle.aduma@pau.edu.ng>
2026-09-07 12:15:18 +0100

    practice 4

---
+++ week-3/practice_3/src/main.rs
@@ -0,0 +1,10 @@
+//floats
+fn main() {
+    let result = 10.00; //f64 by default
+    let interest:f32 = 8.35;
+    let cost:f64 = 15000.600; //double precision
+
+    println!("Result is {}", result);
+    println!("Interest is {}", interest);
+    println!("Cost is {}", cost);
+}
